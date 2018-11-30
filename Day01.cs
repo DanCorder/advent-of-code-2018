211646c83841b8bd9a60c9101de83a70ec8506e5
Dan Corder <dev@dancorder.com>
2018-11-30 21:50:20 +0000

    feat: Initial commit

---
+++ Day01.cs
@@ -0,0 +1,24 @@
+namespace advent_of_code_2018
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Day01
+    {
+        private const string Problem1Input = "";
+
+
+        public static int SolveProblem1()
+        {
+            var total = 0;
+            return total;
+        }
+
+        public static int SolveProblem2()
+        {
+            var total = 0;
+            return total;
+        }
+    }
+}
---
+++ Template.cs
@@ -0,0 +1,21 @@
+namespace advent_of_code_2018
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class DayQQ
+    {
+        private const string Problem1Input = @"";
+
+        public static int SolveProblem1()
+        {
+            return 0;
+        }
+
+        public static int SolveProblem2()
+        {
+            return 0;
+        }
+    }
+}
