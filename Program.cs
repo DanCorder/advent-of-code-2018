dff6fc9fd696ca426ae46340af0e71e422f505a5
Dan Corder <dev@dancorder.com>
2018-12-20 20:33:38 +0000

    Day 20

--- Program.cs
+++ Program.cs
@@ -44,9 +44,9 @@ namespace advent_of_code_2018
             // Console.WriteLine(Day18.SolveProblem1());
             // Console.WriteLine(Day18.SolveProblem2());
             // Console.WriteLine(Day19.SolveProblem1());
-            Console.WriteLine(Day19.SolveProblem2());
+            // Console.WriteLine(Day19.SolveProblem2());
             // Console.WriteLine(Day20.SolveProblem1());
-            // Console.WriteLine(Day20.SolveProblem2());
+            Console.WriteLine(Day20.SolveProblem2());
             // Console.WriteLine(Day21.SolveProblem1());
             // Console.WriteLine(Day21.SolveProblem2());
             // Console.WriteLine(Day22.SolveProblem1());
